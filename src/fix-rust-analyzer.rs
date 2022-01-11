0e6b1aa3d670eecd1fafcfc4ee29555d06b92c04
Jack Clayton <jackclayto@gmail.com>
2022-01-11 23:16:36 +0800

    Fix up rust analyzer fixer

--- src/fix-rust-analyzer.rs
+++ src/fix-rust-analyzer.rs
@@ -1,11 +1,54 @@
-// use std::fs;
 use glob::glob;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct RustProject {
+    sysroot_src: String,
+    crates: Vec<Crate>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Crate {
+    root_module: String,
+    edition: String,
+    deps: Vec<String>,
+}
+
+impl RustProject {
+    fn new() -> RustProject {
+        RustProject {
+            sysroot_src: RustProject::get_sysroot_src(),
+            crates: Vec::new(),
+        }
+    }
+
+    fn get_sysroot_src() -> String {
+        let mut sysroot_src = home::rustup_home()
+            .expect("Can't find Rustup... aborting")
+            .to_string_lossy()
+            .to_string();
+
+        use std::process::Command;
+        let output = Command::new("rustup")
+            .arg("default")
+            .output()
+            .expect("Failed to get rustup default toolchain");
+
+        let toolchain = String::from_utf8_lossy(&output.stdout).to_string();
+
+        sysroot_src += "/toolchains/";
+        sysroot_src += toolchain
+            .split_once(' ')
+            .expect("Malformed default toolchain path")
+            .0;
+        sysroot_src += "/lib/rustlib/src/rust/library";
+        println!("{}", sysroot_src);
+        sysroot_src
+    }
+}
 
 fn main() {
-    use std::env;
-    let out_dir = env::var("RUSTC").expect("RUSTC not there");
-    println!("{}", out_dir);
-    let mut crates = String::new();
+    let mut project = RustProject::new();
     for e in glob("./exercises/**/*").expect("Glob failed to read pattern") {
         let path = e
             .expect("Unable to extract path")
@@ -13,34 +56,17 @@ fn main() {
             .to_string();
         if let Some((_, ext)) = path.split_once(".") {
             if ext == "rs" {
-                crates.push_str(
-                    format!(
-                        r#"{{
-    "root_module": "{}",
-    "edition": "2021",
-    "deps": []
-}}, 
-"#,
-                        &path
-                    )
-                    .as_str(),
-                );
+                project.crates.push(Crate {
+                    deps: Vec::new(),
+                    edition: "2021".to_string(),
+                    root_module: path,
+                })
             }
         }
     }
-    let project = format!(
-        r#"{{
-	"sysroot_src": "/home/jacko/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/library",
-	"crates": [
-            {}
-            ]
-        }}"#,
-        crates
-    );
-    std::fs::write("./rust-project.json", project).expect("Failed to write file");
-    // let paths = fs::File::("./exercises").unwrap();
-
-    // for path in paths {
-    // println!("Name: {}", path.unwrap().path().display());
-    // }
+    std::fs::write(
+        "./rust-project.json",
+        serde_json::to_vec(&project).expect("Failed to serialize to JSON"),
+    )
+    .expect("Failed to write file");
 }
