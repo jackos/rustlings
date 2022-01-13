8bd891a0961738fcdd02559f5aa55b1f12c14c23
Jack Clayton <jackclayto@gmail.com>
2022-01-13 22:09:35 +0800

    Options modules generics traits

--- exercises/traits/traits1.rs
+++ exercises/traits/traits1.rs
@@ -15,7 +15,11 @@ trait AppendBar {
 }
 
 impl AppendBar for String {
-    //Add your code here
+    fn append_bar(self) -> Self {
+        let mut x = self;
+        x.push_str("Bar");
+        x
+    }
 }
 
 fn main() {
